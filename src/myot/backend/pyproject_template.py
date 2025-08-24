d63d19c8b177890c1dd8a83b4c8fe18e74b8ad5c
Hg03 <gehloth03@gmail.com>
2025-08-24 09:44:17 +0000

    theme changed

--- src/myot/backend/pyproject_template.py
+++ src/myot/backend/pyproject_template.py
@@ -1,7 +1,7 @@
 pyproject_prompt_template = """Assume you are a pyproject.toml expert. Generate a pyproject.toml file based on the specific information provided by the user. This pyproject will be a uv package based manager.
 I want my project to be named {project_name} and also provide {description} as provided and I want to use Python {python_version}. 
 I want to include the following dependencies: {dependencies}, if there are no dependencies specified, keep the list empty. 
-Also add the hatchling part in the pyproject and include the {project_name} at packages parameter also. Don't include any extra explanations or something, just give me pyproject.toml code only.
+Also add the hatchling part in the pyproject and include the src/{project_name} at packages parameter also. Don't include any extra explanations or something, just give me pyproject.toml code only.
 Take an example of below pyproject.toml.
 [project]
 name = "{project_name}"
